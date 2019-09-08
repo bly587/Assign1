f67256e14d33bde36d5cf06715b5f4e11fa59f12
tmitchell <tmitchell@chapman.edu>
2019-09-08 20:12:25 +0000

    Input and file

---
+++ assign1.cpp
@@ -0,0 +1,30 @@
+/*
+Titan Mitchell
+2327159
+tmitchell@chapman.edu
+CPSC 350 - 02
+Assignment "HelloWorld"
+
+Executes a program that prints out "Hello World" to the user
+*/
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argr)
+{
+  string fileName = "";
+  if(argr[1] != NULL)
+  {
+    fileName = argr[1];
+  }
+  else
+  {
+    cout << "Please enter a file" << endl;
+    return 1;
+  }
+
+  cout << fileName << endl;
+
+}
